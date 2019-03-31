d31befd20436a2ee034a20b1d7dee1f656ded44b
bgjones15 <bgjones15@winona.edu>
2019-03-31 13:59:24 -0500

    Changes to sensor model and input

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ window.onload = function(){
 
     var counter = 0;
     var openCounter = 0;
+    var bitInput = 0;
     
     var transition = [[0,1/2,1/2,0,0,0,0],[1,0,0,0,0,0,0],[1/2,0,0,0,1/2,0,0],[0,0,0,0,0,0,1],[0,0,1/2,0,0,1/2,0],[0,0,0,0,1/2,0,1/2],[0,0,0,1/2,0,1/2,0]];
     
@@ -20,6 +21,11 @@ window.onload = function(){
     for(var i = 0; i<16; i++){
         observed[i] = new Array(7);
     }
+    for(var i = 0; i<16; i++){
+        for(var j = 0; j<7; j++){
+            observed[i,j] = new Array(7);
+        }
+    }
     
     console.log(transition);
     console.log(observed);
@@ -28,7 +34,22 @@ window.onload = function(){
     var submit = document.getElementById("submit");
     submit.onclick = function(){
         var input = document.getElementById("inputText").value.toUpperCase().split(" ");
-        console.log(input);
+        input.forEach(function(elem){
+        if(elem.includes("N")){
+            bitInput += 1000;
+        }
+        if(elem.includes("S")){
+            bitInput += 100;
+        }
+        if(elem.includes("E")){
+            bitInput += 10;
+        }
+        if(elem.includes("W")){
+            bitInput += 1;
+        }
+        console.log(bitInput);
+        bitInput = 0;
+    });
     }
     
 };
