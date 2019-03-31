fa64e97a3e1755d58115819f8313d58ca65f9a86
scrbmuffin <bbwe24@gmail.com>
2019-03-31 16:11:10 -0500

    Update script.js

--- script.js
+++ script.js
@@ -4,12 +4,8 @@ window.onload = function(){
     var openCounter = 0;
     var bitInput = 0;
     
-    var transition = [[0,1/2,1/2,0,0,0,0],[1,0,0,0,0,0,0],[1/2,0,0,0,1/2,0,0],[0,0,0,0,0,0,1],[0,0,1/2,0,0,1/2,0],[0,0,0,0,1/2,0,1/2],[0,0,0,1/2,0,1/2,0]];
-    
-    var observed = new Array();
-    observed[0] = new Array();
-    observed[0,0] = new Array();
-    observed[0,0,0] = "hello";
+    var transition = [[0,1/2,1/2,0,0,0,0],[1,0,0,0,0,0,0],[1/2,0,0,0,1/2,0,0],[0,0,0,0,0,0,1],[0,0,1/2,0,0,1/2,0],[0,0,0,0,1/2,0,1/2],[0,0,0,1/2,0,1/2,0]];    
+    var locations = [[1,1,0],[1,0,1],[1,1,1]]
     
     var blocks = document.getElementsByTagName("td");
     Array.from(blocks).forEach(function(elem){
@@ -17,7 +13,8 @@ window.onload = function(){
             elem.id = counter++;
         }
     });
-    observed = new Array(16);
+    
+    var observed = new Array(16);
     for(var i = 0; i<16; i++){
         observed[i] = new Array(7);
     }
@@ -26,6 +23,10 @@ window.onload = function(){
             observed[i,j] = new Array(7);
         }
     }
+    var actual = new Array(7);
+    actual = initialize_actual(locations);
+    console.log(actual);
+    console.log(update_sensor_model("1010"))
     
     console.log(transition);
     console.log(observed);
@@ -36,13 +37,13 @@ window.onload = function(){
         var input = document.getElementById("inputText").value.toUpperCase().split(" ");
         input.forEach(function(elem){
         if(elem.includes("N")){
-            bitInput += 1000;
+            bitInput += 8;
         }
         if(elem.includes("S")){
-            bitInput += 100;
+            bitInput += 4;
         }
         if(elem.includes("E")){
-            bitInput += 10;
+            bitInput += 2;
         }
         if(elem.includes("W")){
             bitInput += 1;
@@ -53,3 +54,45 @@ window.onload = function(){
     }
     
 };
+
+function initialize_actual(given_locations){
+    
+    var return_actual = new Array(7);
+    var counter = 0;
+    var value;
+    
+    for (var i = 0; i < 3; i++){
+        for (var j = 0; j < 3; j++){
+            if (given_locations[i][j] == 1){
+                value = 0;
+                if ((i - 1) != -1 && given_locations[i-1][j] == 1){
+                    return_actual[counter] += 8;
+                }
+                if ((i + 1) != 3 && given_locations[i+1][j] == 1){
+                    return_actual[counter] += 4;
+                }
+                if ((j + 1) != 3 && given_locations[i][j+1] == 1){
+                    return_actual[counter] += 2;
+                }
+                if ((j - 1) != -1 && given_locations[i][j-1] == 1){
+                    return_actual[counter] += 1;
+                }
+                counter++;                
+            }
+        }
+    }
+    return return_actual; 
+}
+
+function update_sensor_model(obs, error){
+    
+    var index = parseInt(obs, 2);
+    var xor = 0;
+    var correct = 0
+    
+    for (var i = 0; i < 7; i++){
+        xor = (index ^ actual[i]).toString(2);
+        correct = 4 - (xor.match(/1/g) || []).length;
+        observation[index][i][i] = ((1 - error)**correct)*(error**(4-correct));
+    }
+}
